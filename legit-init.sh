1f93e345449ee99c773992ceda6ecb31f4489f90
z5232974 <z5232974@cse.unsw.edu.au>
2019-07-03 21:02:19 +1000

    Update commit implementation and add log and commit to init

--- legit-init.sh
+++ legit-init.sh
@@ -1,30 +1,63 @@
 #!/bin/dash
 
-if test -d '.legit'
+if [ ! $# -eq 2 ] && [ ! $# -eq 3 ]
 then
-    echo "legit-init: error: .legit already exists"
+    echo 'usage: legit-commit [-a] -m commit-message'
     exit 1
 fi
 
-if mkdir '.legit'
+a=0
+m=0
+if [ "$1" = '-a' ]
 then
-    echo "Initialized empty legit repository in .legit"
+    a=1
+elif [ "$1" = '-m' ] && [ "$#" -eq 2 ]
+then
+    m=1
+else
+    echo 'usage: legit-commit [-a] -m commit-message'
+    exit 1
 fi
 
-# Creating internal directories
-mkdir '.legit/objects'
-mkdir '.legit/refs'
-touch '.legit/index'
+if [ $# -eq 3 ]
+then
+    mess="$3"
+    if [ "$2" = '-a' ]
+    then
+        echo 'usage: legit-commit [-a] -m commit-message'
+        exit 1
+    elif [ $m -eq 0 ] && [ "$2" = '-m' ]
+    then
+        m=1
+    else
+        echo 'usage: legit-commit [-a] -m commit-message'
+        exit 1
+    fi
+else
+    mess="$2"
+fi
 
-mkdir '.legit/branch'
-touch '.legit/branch/master'
+# Add everything from index to commit
+if [ "$a" = 1 ]
+then
+    blobs=`cat .legit/index`    
+fi
 
-# Initiate reference to HEAD as master
-echo 'ref: refs/heads/master' >'.legit/HEAD'
+# Update legit-log
+# Add number commit into log with message
+num=`cat '.legit/log' | egrep -o '^[0-9]*' | head -1`
+if [ ! $num ]
+then
+    # start at 0 commit
+    num=0
+    echo "$num $mess" >>'.legit/log'
+else
+    num=`expr $num + 1`
+    sed -i "1 i\\$num $mess" '.legit/log'
+fi
 
 
-# Store the worktree path
-wd=`pwd`
-echo "$wd" >'.legit/config'
+
+
 
 exit 0
